dc6f452b73b08580d1c3427ba9ba6f1555d49aa9
ZR Saimun <zrsaimun@gmail.com>
2020-11-17 04:44:09 +0600

    Customer Login Query

---
+++ Online Computer Shop/model/customerModel.js
@@ -0,0 +1,23 @@
+var db = require('./db');
+
+module.exports = {
+    get: function(customerId, callback) {
+        var sql = "select * from customers where M_ID = ?";
+
+        db.getResult(sql, [customerId], function(result) {
+            callback(result);
+        });
+    },
+    getAll: function(callback) {
+        var sql = "select * from customers";
+        db.getResult(sql, [], function(results) {
+            callback(results);
+        });
+    },
+    insert: function(customer, callback) {
+        var sql = "insert into customers values (?, ?, ?, ?)";
+        db.execute(sql, [customer.id, customer.name, customer.address, customer.email], function(status) {
+            callback(status);
+        });
+    },
+}
