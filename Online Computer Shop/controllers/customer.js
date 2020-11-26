fa8a720f6a8feedabb5d4e9e54dba9fe0e7c1c7b
ZR Saimun <zrsaimun@gmail.com>
2020-11-26 23:15:25 +0600

    complete with AJAX

--- Online Computer Shop/controllers/customer.js
+++ Online Computer Shop/controllers/customer.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var adminModel = require.main.require('./model/adminModel');
 var customerModel = require.main.require('./model/customerModel');
+var shopModel = require.main.require('./model/shopModel');
+var productModel = require.main.require('./model/productModel');
+var productReviewModel = require.main.require('./model/productReviewModel');
+
+var router = express.Router();
+
 
 var router = express.Router();
 
@@ -11,6 +18,24 @@ router.get('*', function(req, res, next) {
     }
 });
 
+router.get('/', (req, res) => {
+    shopModel.getAll(function(results) {
+        if (results.length > 0) {
+            var shops = {
+                name: req.session.uId,
+                shopList: results
+            };
+            res.render('customer/index', shops);
+        } else {
+            var shops = {
+                name: req.session.uId,
+                shopList: ""
+            };
+            res.render('customer/index', shops);
+        }
+    });
+});
+
 router.get('/profile', (req, res) => {
     customerModel.get(req.session.uId, function(result) {
         if (result.length > 0) {
@@ -18,6 +43,82 @@ router.get('/profile', (req, res) => {
         }
     });
 });
+
+
+//! customer review product
+
+router.get('/shop/:shopId', (req, res) => {
+    var shopDetails;
+    shopModel.get(req.params.shopId, function(result) {
+        if (result.length > 0) {
+            shopDetails = result[0];
+            productModel.getAllByShopId(req.params.shopId, function(results) {
+                if (results.length > 0) {
+                    var computer = {
+                        shop: shopDetails,
+                        computerList: results
+                    };
+                    res.render('customer/shopComputer', computer);
+                } else {
+                    var computer = {
+                        shop: shopDetails,
+                        computerList: ""
+                    };
+                    res.render('customer/shopComputer', computer);
+                }
+            });
+        }
+    });
+});
+
+router.get('/shop/:shopId/computer/review/:productId', (req, res) => {
+    console.log("db product review");
+    console.log(req.params.productId);
+    productReviewModel.getByProductId(req.params.productId, function(results) {
+        if (results.length > 0) {
+            console.log("get product review");
+            var comments = {
+                productId: req.params.productId,
+                commentList: results
+            };
+            res.render('customer/productReview', comments);
+        } else {
+            console.log("not get product review");
+            var comments = {
+                productId: req.params.productId,
+                commentList: ""
+            }
+            res.render('customer/productReview', comments);
+        }
+    });
+});
+
+//! AJAX
+router.get('/productReview/:productId/:commentText', (req, res) => {
+    var comment = {
+        customerId: req.session.uId,
+        text: req.params.commentText,
+        time: new Date(),
+        productId: req.params.productId
+    };
+    console.log("ajaxxx");
+
+    console.log(comment.productId);
+    productReviewModel.insert(comment, function(success) {
+        if (success) {
+            console.log("ajax done");
+            productReviewModel.getByProductId(req.params.productId, function(results) {
+                if (results.length > 0) {
+                    res.send(results);
+                } else {
+                    res.send(results);
+                }
+            });
+        }
+    });
+
+});
+
 
 
 
